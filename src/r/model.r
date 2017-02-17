f6fedb7a9e18d1f974f89a4fad4513a873221e28
Dwivedi <Pawan.Dwivedi@echostar.com>
2017-02-17 21:22:33 +0530

    Add metadata

--- src/r/model.r
+++ src/r/model.r
@@ -1,6 +1,12 @@
 source("../../src/r/main.r")
 
-genre <- read.csv("../../data/movie_genre.tsv", sep = '\t', header = FALSE, quote = "")
+genre <- read.csv("../../data/genre_2016.tsv", sep = '\t', header = TRUE, quote = "")
 
-names(genre) <- c("title", "genre")
-genre <- right_join(genre, movie.country.score, by = "title")
+
+movie.country.score.genre <- merge(movie.country.score, genre, by = "title", all.x = TRUE)
+
+movie.country.score.genre <- left_join(movie.country.score.genre, total.gross.title, by = "title")
+
+movie.country.score.genre <- left_join(movie.country.score.genre, total.gross.country, by = "country")
+
+write.table(movie.country.score.genre, file = "../../output/data/main_data.tsv")
